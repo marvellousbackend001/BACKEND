dc336fe2b9eec65668c5a16b81cd3f3c04ef08de
marvellous <kennedy marvellous001@gmail.com>
2024-03-14 09:36:33 +0100

    i installed nodemailer

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const cors = require("cors");
 
 const app = express();
 const con = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    password: "CHIDERA001?.1",
-    database: "test2",
-    port: "3306",
+  host: "127.0.0.1",
+  user: "root",
+  password: "CHIDERA001?.1",
+  database: "test2",
+  port: "3306",
 });
 
 
@@ -17,29 +17,29 @@ const con = mysql.createConnection({
 
 app.post("/signup", bodyparser.json(), function (req, res) {
 
-    var sql = `INSERT INTO users(firstname,lastname,email,gender,pass)
+  var sql = `INSERT INTO users(firstname,lastname,email,gender,pass)
     VALUES('${req.body.firstname}','${req.body.lastname}','${req.body.email}','${req.body.gender}','${req.body.pass}')`;
-    con.query(sql, function (err, result) {
-        if (err) throw err
-        res.send(result);
-    });
+  con.query(sql, function (err, result) {
+    if (err) throw err
+    res.send(result);
+  });
 });
 
 app.get("/login", bodyparser.json(), function (req, res) {
-    var sql = `SELECT * FROM users 
+  var sql = `SELECT * FROM users 
     WHERE email ='${req.body.email}' AND pass ='${req.body.pass}' `
-    con.query(sql, function (err, result) {
-        if (err) throw err
-        res.send(result);
-    });
+  con.query(sql, function (err, result) {
+    if (err) throw err
+    res.send(result);
+  });
 });
 app.get("/getuser/:id", bodyparser.json(), function (req, res) {
-    var sql =`SELECT *FROM users
+  var sql = `SELECT *FROM users
     WHERE email ='${req.params.id}'`
-    con.query(sql, function (err, result) {
-        if (err) throw err
-        res.send(result);
-    });
+  con.query(sql, function (err, result) {
+    if (err) throw err
+    res.send(result);
+  });
 });
 app.listen(8000, console.log("listening 8000"));
 
@@ -48,36 +48,36 @@ app.listen(8000, console.log("listening 8000"));
 const nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'kennedymarvellous001@gmail.com',
-      pass: 'zceb znrq ajzh wszq'
-    }
-  });
-  
-  var mailOptions = (from,to,subject,body,)
+  service: 'gmail',
+  auth: {
+    user: 'kennedymarvellous001@gmail.com',
+    pass: 'zceb znrq ajzh wszq'
+  }
+});
 
-    
-  
-  app.post('/send-email', (req, res) => {
-    const { to, subject, body, recieveremailadress,fullname } = req.body;
-    if (err) throw err
-    res.send(result);
-  })
-  
+app.post('/send-email', function (req, res) {
+  const { from, to, subject, body } = req.body;
+
+  var mailOptions = {
+    from,
+    to,
+    subject,
+    body
+ };
+
   transporter.sendMail(mailOptions, function (error, info) {
+   
     if (error) {
       console.log(error);
     } else {
       console.log('Email sent: ' + info.response);
     }
   });
-  
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-   console.log(`Server running on port ${port}`);
-  });
-  
-  
-  
-  
+})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+
+
